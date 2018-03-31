374f9421335acd7f05974f408c8180de1a50e171
a00908270 <a00908270@unet.univie.ac.at>
2018-03-31 19:25:29 +0200

    vinnsl->dl4j mapper
    map vinnsl definition parameters using mapstruct to dl4j attributes
    * learningrate -> learningRate
    * momentum -> momentum
    * biasInput -> biasInit
    * epochs -> numIterations
    * threshold -> gradientNormalizationThreshold
    * activationfunction -> (ENUM) activationFn

---
+++ src/main/java/at/ac/univie/a00908270/nnworker/vinnsl/transformation/MappingUtil.java
@@ -0,0 +1,148 @@
+package at.ac.univie.a00908270.nnworker.vinnsl.transformation;
+
+import at.ac.univie.a00908270.vinnsl.schema.Parametervalue;
+import org.mapstruct.Qualifier;
+import org.nd4j.linalg.activations.IActivation;
+import org.nd4j.linalg.activations.impl.ActivationSigmoid;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class MappingUtil {
+	
+	// learningrate -> learningRate
+	// momentum -> momentum
+	// biasInput -> biasInit
+	// epochs -> numIterations
+	// threshold -> gradientNormalizationThreshold
+	// activationfunction -> (ENUM) activationFn
+	
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface LearningRate {
+	}
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface Momentum {
+	}
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface BiasInit {
+	}
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface NumIterations {
+	}
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface GradientNormalizationThreshold {
+	}
+	
+	@Qualifier
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface ActivationFn {
+	}
+	
+	@LearningRate
+	public Double learningRate(java.util.List<java.lang.Object> in) {
+		Parametervalue.Valueparameter param = ((Parametervalue.Valueparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Valueparameter)
+				.filter(e -> ((Parametervalue.Valueparameter) e).getName().equals("learningrate"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			return param.getValue().doubleValue();
+		}
+		
+		return 0.1;
+	}
+	
+	@Momentum
+	public Double momentum(java.util.List<java.lang.Object> in) {
+		Parametervalue.Valueparameter param = ((Parametervalue.Valueparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Valueparameter)
+				.filter(e -> ((Parametervalue.Valueparameter) e).getName().equals("momentum"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			return param.getValue().doubleValue();
+		}
+		
+		return 0d;
+	}
+	
+	@BiasInit
+	public Double biasInit(java.util.List<java.lang.Object> in) {
+		
+		Parametervalue.Valueparameter param = ((Parametervalue.Valueparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Valueparameter)
+				.filter(e -> ((Parametervalue.Valueparameter) e).getName().equals("biasInput"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			return param.getValue().doubleValue();
+		}
+		
+		return 0d;
+	}
+	
+	@NumIterations
+	public Double numIterations(java.util.List<java.lang.Object> in) {
+		
+		Parametervalue.Valueparameter param = ((Parametervalue.Valueparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Valueparameter)
+				.filter(e -> ((Parametervalue.Valueparameter) e).getName().equals("epochs"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			return param.getValue().doubleValue();
+		}
+		
+		return 0d;
+	}
+	
+	@GradientNormalizationThreshold
+	public Double gradientNormalizationThreshold(java.util.List<java.lang.Object> in) {
+		
+		Parametervalue.Valueparameter param = ((Parametervalue.Valueparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Valueparameter)
+				.filter(e -> ((Parametervalue.Valueparameter) e).getName().equals("threshold"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			return param.getValue().doubleValue();
+		}
+		
+		return 0d;
+	}
+	
+	@ActivationFn
+	public IActivation activationFn(java.util.List<java.lang.Object> in) {
+		
+		Parametervalue.Comboparameter param = ((Parametervalue.Comboparameter) (in.stream()
+				.filter(e -> e instanceof Parametervalue.Comboparameter)
+				.filter(e -> ((Parametervalue.Comboparameter) e).getName().equals("activationfunction"))
+				.findFirst().orElse(null)));
+		
+		if (param != null) {
+			if ("sigmoid".equalsIgnoreCase(param.getValue())) {
+				return new ActivationSigmoid();
+			}
+		}
+		
+		return null;
+	}
+}
--- src/main/java/at/ac/univie/a00908270/nnworker/vinnsl/transformation/VinnslDL4JMapper.java
+++ src/main/java/at/ac/univie/a00908270/nnworker/vinnsl/transformation/VinnslDL4JMapper.java
@@ -7,17 +7,36 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = MappingUtil.class)
 public interface VinnslDL4JMapper {
 	VinnslDL4JMapper INSTANCE = Mappers.getMapper(VinnslDL4JMapper.class);
 	
-	/*	@Mapping(source = "activationfunction", target = "activation")
-		NeuralNetConfiguration.Builder neuralNetConfiguration(Instanceschema instance);*/
-	@Mappings({
+	/*@Mapping(source = "activationfunction", target = "activation")
+	NeuralNetConfiguration.Builder neuralNetConfiguration(Instanceschema instance);*/
+	
+	/*@Mappings({
 			@Mapping(source = "vinnsl.trainingresult.learningrate", target = "learningRate"),
 			@Mapping(source = "vinnsl.trainingresult.epochs", target = "numIterations"),
 			//@Mapping(source = "vinnsl.instance.activationfunction", target = "activationFn"),
 			@Mapping(source = "vinnsl.instance.weightmatrix", target = "weightInit")
+	})*/
+	/*NeuralNetConfiguration.Builder neuralNetConfiguration(Vinnsl vinnsl);*/
+	
+	@Mappings({
+			// learningrate -> learningRate
+			// momentum -> momentum
+			// biasInput -> biasInit
+			// epochs -> numIterations
+			// threshold -> gradientNormalizationThreshold
+			// activationfunction -> (ENUM) activationFn
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "learningRate", qualifiedBy = MappingUtil.LearningRate.class),
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "momentum", qualifiedBy = MappingUtil.Momentum.class),
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "biasInit", qualifiedBy = MappingUtil.BiasInit.class),
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "numIterations", qualifiedBy = MappingUtil.NumIterations.class),
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "gradientNormalizationThreshold", qualifiedBy = MappingUtil.GradientNormalizationThreshold.class),
+			@Mapping(source = "vinnsl.definition.parameters.valueparameterOrBoolparameterOrComboparameter", target = "activationFn", qualifiedBy = MappingUtil.ActivationFn.class)
 	})
 	NeuralNetConfiguration.Builder neuralNetConfiguration(Vinnsl vinnsl);
+	
+	
 }
